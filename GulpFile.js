a5c41a41b03e410bad742bc88e6d2a371b66cf5c
antronic <antronic.inc@gmail.com>
2016-08-13 03:17:38 +0700

    change views engine to EJS, use GridsterJS, create new layout

--- GulpFile.js
+++ GulpFile.js
@@ -5,10 +5,12 @@ var minifycss = require('gulp-minify-css');
 var browserSync = require('browser-sync');
 
 var sass_input = './src/sass/*.+(scss|sass)';
-var css_output = './assets/css/';
-var view_dir = './public/*.+(html|htm)';
+var bootstrap = './src/bootstrap-sass/assets/stylesheets/bootstrap.scss';
+var all_boostrap = './src/bootstrap-sass/assets/stylesheets/**/**/*.+(scss|sass)';
+var css_output = './public/assets/css/';
+var view_dir = './views/*.+(ejs)';
 
-var port = 3000;
+var port = 5000;
 var ip = '127.0.0.1';
 var proxy = ip+":"+port;
 
@@ -24,12 +26,25 @@ gulp.task('watch', function(){
     });
 
     gulp.watch(sass_input, ['sass']);
+    gulp.watch(bootstrap, ['bootstrap-sass']);
     gulp.watch(view_dir).on('change', browserSync.reload);
 });
 
 gulp.task('sass', function(){
     return gulp
         .src(sass_input)
+        .pipe(sass(sass_options).on('error', sass.logError))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(minifycss())
+        .pipe(gulp.dest(css_output))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('bootstrap-sass', function(){
+    return gulp
+        .src(bootstrap)
         .pipe(sass(sass_options).on('error', sass.logError))
         .pipe(rename({
             suffix: '.min'
--- server.js
+++ server.js
@@ -3,11 +3,24 @@ var app = express();
 // var serverHTTPS = require('https').createServer();
 var server = require('http').createServer(app);
 var path = require('path');
+var bodyParser = require('body-parser');
 
-server.listen(3000);
+server.listen(5000);
 
+// parse various different custom JSON types as JSON
+app.use(bodyParser.json({ type: 'application/*+json' }))
+
+// parse some custom thing into a Buffer
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+
+// parse an HTML body into a string
+app.use(bodyParser.text({ type: 'text/html' }))
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
+
 app.get('/', function(req, res){
-    
+    res.render('index');
 });
