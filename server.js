f1f7266554ea7c638cee03ef721f6e668016bae5
KANISORN <firstziiz.k@gmail.com>
2016-08-18 18:11:49 +0700

    init schedule

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var server = require('http').createServer(app);
 var path = require('path');
 var bodyParser = require('body-parser');
 
-server.listen(5000);
+server.listen(3000);
 
 // parse various different custom JSON types as JSON
 app.use(bodyParser.json({ type: 'application/*+json' }))
