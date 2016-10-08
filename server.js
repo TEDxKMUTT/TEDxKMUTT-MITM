57c856fc15d6b66dd7ed5cf99321566402985fd5
antronic <antronic.inc@gmail.com>
2016-10-08 14:31:32 +0700

    response port

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ var server = require('http').createServer(app);
 var path = require('path');
 var bodyParser = require('body-parser');
 
-server.listen(5000);
+server.listen(5000, function(){
+    console.log("listening on ", server.address().port);
+});
 
 // parse various different custom JSON types as JSON
 app.use(bodyParser.json({ type: 'application/*+json' }))
